f5da678fd6474586c036e0ab26f695b06e625b5b
Romain-Constant <romain.constant59@gmail.com>
2023-06-14 20:18:40 +0200

    practitioner desktop layout created

--- frontend/src/components/practitionner/practitionerLayout/PractitionerLayout.jsx
+++ frontend/src/components/practitionner/practitionerLayout/PractitionerLayout.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
-import styles from "./PractitionerLayout.module.css";
 import { Outlet } from "react-router-dom";
 import PratictionnerNavbarMobile from "../PratictionnerNavbarMobile/PratictionnerNavbarMobile";
 import PratictionnerHeaderDesktop from "../pratictionnerHeaderDesktop/PratictionnerHeaderDesktop";
 import PratictionnerHeaderMobile from "../pratictionnerHeaderMobile/PratictionnerHeaderMobile";
 import PratictionnerNavbarDesktop from "../pratictionnerNavbarDesktop/PratictionnerNavbarDesktop";
+import styles from "./PractitionerLayout.module.css";
 
 function PractitionerLayout() {
   const isDesktop = useMediaQuery({ query: "(min-width: 991px)" });
