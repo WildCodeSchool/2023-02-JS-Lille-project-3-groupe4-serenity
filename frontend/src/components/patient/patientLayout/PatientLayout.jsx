c9baac8569a7340fdfe14deb68c8af884251b488
Romain-Constant <romain.constant59@gmail.com>
2023-07-04 16:54:56 +0200

    navbar mobile and burger menu done

--- frontend/src/components/patient/patientLayout/PatientLayout.jsx
+++ frontend/src/components/patient/patientLayout/PatientLayout.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { Outlet } from "react-router-dom";
+import { FaRegWindowClose } from "react-icons/fa";
 import PatientHeaderDesktop from "../patientHeaderDesktop/PatientHeaderDesktop";
 import PatientHeaderMobile from "../patientHeaderMobile/PatientHeaderMobile";
 import PatientNavbarDesktop from "../patientNavbarDesktop/PatientNavbarDesktop";
@@ -10,6 +11,7 @@ import styles from "./PatientLayout.module.css";
 function PatientLayout() {
   const isDesktop = useMediaQuery({ query: "(min-width: 991px)" });
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 990px)" });
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   return (
     <div>
@@ -26,11 +28,35 @@ function PatientLayout() {
       )}
       {isTabletOrMobile && (
         <div className={styles.patientLayoutMobileContainer}>
+          {isMenuOpen && (
+            <div
+              className={`${styles.menuContainer} ${isMenuOpen ? "open" : ""}`}
+            >
+              <div className={styles.closeContainer}>
+                <FaRegWindowClose
+                  className={styles.closeIcon}
+                  onClick={() => setMenuOpen(false)}
+                />
+              </div>
+              <div className={styles.linkContainer}>
+                <h2 className={styles.linkText}>Comprendre mon opération</h2>
+                <h2 className={styles.linkText}>Formalités administratives</h2>
+                <h2 className={styles.linkText}>Préparer mon arrivée</h2>
+                <h2 className={styles.linkText}>Anticiper ma sortie</h2>
+                <h2 className={styles.linkText}>Ma checklist</h2>
+                <div className={styles.separator} />
+                <h2 className={styles.linkText}>Test</h2>
+              </div>
+            </div>
+          )}
           <PatientHeaderMobile />
           <div className={styles.outletMobileContainer}>
             <Outlet />
           </div>
-          <PatientNavbarMobile />
+          <PatientNavbarMobile
+            isMenuOpen={isMenuOpen}
+            setMenuOpen={setMenuOpen}
+          />
         </div>
       )}
     </div>
--- frontend/src/components/patient/patientNavbarMobile/PatientNavbarMobile.jsx
+++ frontend/src/components/patient/patientNavbarMobile/PatientNavbarMobile.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { FaHome, FaRegBell, FaMusic, FaRocketchat } from "react-icons/fa";
+import { MdOutlineMenu } from "react-icons/md";
+import PropTypes from "prop-types";
 import styles from "./NavbarMobile.module.css";
 
-function PatientNavbarMobile() {
-  return <div className={styles.navbarContainer}>PatientNavbarMobile</div>;
+function PatientNavbarMobile({ isMenuOpen, setMenuOpen }) {
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.navbarIconContainer}>
+        <FaHome className={styles.menuIcon} />
+      </div>
+      <div className={styles.navbarIconContainer}>
+        <FaRegBell className={styles.menuIcon} />
+      </div>
+      <div className={styles.navbarChatContainer}>
+        <FaRocketchat className={styles.menuChatIcon} />
+      </div>
+      <div className={styles.navbarIconContainer}>
+        <FaMusic className={styles.menuIcon} />
+      </div>
+      <div className={styles.navbarIconContainer}>
+        <MdOutlineMenu
+          className={styles.menuIcon}
+          onClick={() => setMenuOpen(!isMenuOpen)}
+        />
+      </div>
+    </div>
+  );
 }
+
+PatientNavbarMobile.propTypes = {
+  isMenuOpen: PropTypes.bool.isRequired,
+  setMenuOpen: PropTypes.bool.isRequired,
+};
 
 export default PatientNavbarMobile;
