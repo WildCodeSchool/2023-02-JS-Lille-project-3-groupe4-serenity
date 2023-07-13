baa9711fe8b40fa08c13bf86d7dae72626fc74ec
Romain-Constant <romain.constant59@gmail.com>
2023-07-13 13:55:39 +0200

    patient desktop understanding page - video modal done

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,12 @@ app.use(express.json());
 
 const cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
 
 // import and mount the API routes
 
