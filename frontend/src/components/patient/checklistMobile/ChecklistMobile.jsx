32b8706f2130878e7dee3f53f8f04762e3a0ee9e
Romain-Constant <romain.constant59@gmail.com>
2023-07-25 16:14:04 +0200

    patient mobile checklist page done

--- frontend/src/components/patient/checklistMobile/ChecklistMobile.jsx
+++ frontend/src/components/patient/checklistMobile/ChecklistMobile.jsx
@@ -1,15 +1,83 @@
 import ProgressBar from "@ramonak/react-progress-bar";
-import React from "react";
-import { BsChevronRight } from "react-icons/bs";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import UnderstepsContext from "../../../contexts/UnderstepsContext";
 import styles from "./ChecklistMobile.module.css";
 
 function ChecklistMobile() {
+  const { countOfOnesUstepFive, setCountOfOnesUstepFive } =
+    useContext(UnderstepsContext);
+
+  const [checkedValues, setCheckedValues] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const [underStepIds, setUnderStepIds] = useState([]);
+  const { idInter } = useParams();
+
+  useEffect(() => {
+    const fetchStep = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/interventions/${idInter}`
+        );
+        const { data } = response;
+
+        const ids = data.map((item) => item.id);
+        const statuts = data.map((item) => item.understepStatut);
+
+        const underStepsSubset = ids.slice(11, 16);
+        const statutsSubset = statuts.slice(11, 16);
+
+        setCheckedValues(statutsSubset);
+        setUnderStepIds(underStepsSubset);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchStep();
+  }, [idInter]);
+
+  const handleChange = (index) => (event) => {
+    const { checked } = event.target;
+
+    setCheckedValues((prevValues) => {
+      const newValues = [...prevValues];
+      newValues[index] = checked;
+      return newValues;
+    });
+
+    const firstFiveUnderStepIds = underStepIds;
+
+    axios
+      .put(`http://localhost:5050/steps/${firstFiveUnderStepIds[index]}`, {
+        statutUnderstep: checked ? 1 : 0,
+      })
+
+      .then(() => {
+        if (checked) {
+          setCountOfOnesUstepFive((prevCount) => prevCount + 1); // Increment onesCountUstepOne by 1 if checkbox is checked
+        } else {
+          setCountOfOnesUstepFive((prevCount) => prevCount - 1); // Decrement onesCountUstepOne by 1 if checkbox is unchecked
+        }
+      })
+      .catch((err) => {
+        console.error("Erreur lors de la mise à jour du statut :", err); // Display the error in the console if the request fails
+      });
+  };
+
   return (
     <div className={styles.checklistMobilePageContainer}>
       <div className={styles.progressBarContainer}>
         <p className={styles.blocTitle}>Anticiper ma sortie</p>
         <ProgressBar
-          completed={60}
+          completed={(countOfOnesUstepFive / 5) * 100}
           maxCompleted={100}
           height="8vh"
           borderRadius="20px"
@@ -20,26 +88,81 @@ function ChecklistMobile() {
       </div>
       <div className={styles.documentsContainer}>
         <div className={styles.document}>
-          <input type="checkbox" />
-          <div className={styles.documentInfosBloc}>
+          <input
+            type="checkbox"
+            checked={checkedValues[0]}
+            onChange={handleChange(0)}
+          />
+          <div
+            className={`${styles.documentInfosBloc} ${
+              checkedValues[0] ? styles.checked : ""
+            }`}
+          >
             <div className={styles.documentName}>Pièce d'identité</div>
             <div className={styles.documentDetails}>Obligatoire</div>
           </div>
-          <div className={styles.rightIconBloc}>
-            <BsChevronRight className={styles.rightIcon} />
+        </div>
+        <div className={styles.document}>
+          <input
+            type="checkbox"
+            checked={checkedValues[1]}
+            onChange={handleChange(1)}
+          />
+          <div
+            className={`${styles.documentInfosBloc} ${
+              checkedValues[1] ? styles.checked : ""
+            }`}
+          >
+            <div className={styles.documentName}>Consultation anesthésique</div>
+            <div className={styles.documentDetails}>Obligatoire</div>
           </div>
         </div>
         <div className={styles.document}>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={checkedValues[2]}
+            onChange={handleChange(2)}
+          />
+          <div
+            className={`${styles.documentInfosBloc} ${
+              checkedValues[2] ? styles.checked : ""
+            }`}
+          >
+            <div className={styles.documentName}>
+              Test COVID (de moins de 3 jours)
+            </div>
+            <div className={styles.documentDetails}>Obligatoire</div>
+          </div>
         </div>
         <div className={styles.document}>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={checkedValues[3]}
+            onChange={handleChange(3)}
+          />
+          <div
+            className={`${styles.documentInfosBloc} ${
+              checkedValues[3] ? styles.checked : ""
+            }`}
+          >
+            <div className={styles.documentName}>Mutuelle</div>
+            <div className={styles.documentDetails}>Facultatif</div>
+          </div>
         </div>
         <div className={styles.document}>
-          <input type="checkbox" />
-        </div>
-        <div className={styles.document}>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={checkedValues[4]}
+            onChange={handleChange(4)}
+          />
+          <div
+            className={`${styles.documentInfosBloc} ${
+              checkedValues[4] ? styles.checked : ""
+            }`}
+          >
+            <div className={styles.documentName}>Carte Vitale</div>
+            <div className={styles.documentDetails}>Obligatoire</div>
+          </div>
         </div>
       </div>
     </div>
